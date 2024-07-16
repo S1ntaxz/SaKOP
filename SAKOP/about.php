3bd95dc9b0d575b9b61beb90b86b13668a37750b
S1ntaxz <21-77271@g.batstate-u.edu.ph>
2024-07-17 03:06:50 +0800

    about page php file

---
+++ SAKOP/about.php
@@ -0,0 +1,59 @@
+<?php include 'logo.php'; ?>
+
+
+<body class="sub_page">
+
+  <div class="hero_area">
+  <?php include 'header.php'; ?>
+  </div>
+
+
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+        </div>
+        <div class="col-md-6">
+          <div class="detail-box">
+            <h2>
+              About Our SaKOP
+            </h2>
+            <p>
+              There are many variations of passages of Lorem Ipsum
+              There are many variations of
+              passages of Lorem Ipsum
+            </p>
+            <a href="">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end about section -->
+
+
+  <?php include 'footer.php'; ?>
+  <?php include 'team.php'; ?>
+
+  <!-- jQery -->
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <!-- popper js -->
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <!-- bootstrap js -->
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <!-- custom js -->
+  <script type="text/javascript" src="js/custom.js"></script>
+  <!-- Google Map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+  </script>
+  <!-- End Google Map -->
+
+</body>
+
+</html>
