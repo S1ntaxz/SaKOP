01f08bc00f9eea176df65c1e7d17a053e6a7806e
S1ntaxz <21-77271@g.batstate-u.edu.ph>
2024-07-17 02:55:00 +0800

    file for team logo

---
+++ SAKOP/team.php
@@ -0,0 +1,10 @@
+ <!-- footer section -->
+ <section class="footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span> All Rights Reserved By
+        <img src="./images/pd.png" alt="Logo" width="80" height="30" class="d-inline-block align-center" loading="lazy">
+      </p>
+    </div>
+  </section>
+  <!-- footer section -->
